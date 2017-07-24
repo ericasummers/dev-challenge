2064ab19b667cb4d9a26d6d1aecab44b6dc38636
Erica Wright <ericaw21@gmail.com>
2017-07-24 12:31:35 -0700

    set up dropdowns and ajax call to convert-wip

--- app/components/converter-form.js
+++ app/components/converter-form.js
@@ -1,8 +1,29 @@
 import Ember from 'ember';
+import ajax from 'ic-ajax';
 
 export default Ember.Component.extend({
     currencyFrom: null,
-    currenciesFrom: Ember.String.w('BTC ETH EUR USD ZEC'),
     currencyTo: null,
-    currenciesTo: Ember.String.w('BTC ETH EUR USD ZEC')
+    currencies: Ember.String.w('BTC ETH EUR USD ZEC'),
+    actions: {
+        convertCurrency() {
+            var From = this.currencyFrom;
+            console.log('currencyFrom choice ', From);
+            var To = this.currencyTo;
+            console.log('currencyTo choice ', To);
+            var amount = this.get('amount');
+            console.log('amount ', amount);
+            var convertToWhat = From.concat(To);
+            ajax({
+                url: 'https://citadel-miner.appspot.com/data/v1/converter?set=devtest',
+                type: 'get'
+            })
+            .then(converterResponse => {
+                var conversionValue = amount * converterResponse.From.quotes.convertToWhat;
+
+                console.log(conversionValue + From);
+                return conversionValue + To;
+            });
+        }
+    }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,16 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
-  actions: {
-    convertCurrency(params) {
-      return https.get('https://citadel-miner.appspot.com/data/v1/converter?set=devtest')
-      .then(converterResponse => {
-
-      });
-      // var newQuestion = this.store.createRecord('question', params);
-      // newQuestion.save();
-      // this.transitionTo('index');
-    }
-  }
 });
